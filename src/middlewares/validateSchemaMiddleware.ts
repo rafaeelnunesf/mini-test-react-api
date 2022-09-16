a573838bf01275802525480945981386886a322c
rafaeelnunes <rafaelnfsq@gmail.com>
2022-09-16 16:44:15 -0300

    feat: create validate Schema Middleware

---
+++ src/middlewares/validateSchemaMiddleware.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+import { ObjectSchema } from "joi";
+import { wrongSchemaError } from "../utils/errorUtils.js";
+
+export function validateSchemaMiddleware(schema: ObjectSchema) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const validation = schema.validate(req.body);
+    if (validation.error) throw wrongSchemaError(validation.error.message);
+
+    next();
+  };
+}
